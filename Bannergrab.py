5b328ad717b40f6d40142a39755ba03c0f86e435
ner0 <43746083+real-ner0@users.noreply.github.com>
2019-12-01 20:18:50 +0530

    Add files via upload

---
+++ Bannergrab.py
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+import socket
+from termcolor import colored
+
+
+def retBanner(host, port):
+    try:
+        socket.setdefaulttimeout(2)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((host, port))
+        banner = sock.recv(1024)
+
+        return banner
+    except:
+        return
+
+
+def main():
+    host = str(input(colored("[*] Enter the target IP: ", 'blue')))
+    p = int(input(colored('[*] Enter the port upper bound: ', 'blue')))
+    for i in range(1, p+1):
+        banner = retBanner(host, i)
+
+        if banner:
+            print(colored(f"[+] {host}:{i}  ->     " + banner.decode("utf-8"), 'green'), end='')
+        else:
+            print(colored(f"[-] {host}:{i}  ->     " + "Unable to Detect", 'red'))
+
+    exit(0)
+
+
+main()
